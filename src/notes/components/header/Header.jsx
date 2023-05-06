c595e5f36c2588ed639281d5242e3fe7d74edc4e
Yuliya Tokareva <ta4tonado@gmail.com>
2023-05-06 17:44:23 +0300

    fixed build error

--- src/notes/components/header/Header.jsx
+++ src/notes/components/header/Header.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import AddNote from '../../svg/AddNote';
-import Plus from '../../svg/plus';
+import Plus from '../../svg/Plus';
 import Trash from '../../svg/Trash';
 import BtnCustom from '../btnCustom/BtnCustom';
 import SearchBox from '../searchBox/SearchBox';
